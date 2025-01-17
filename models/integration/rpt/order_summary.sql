fbf188f632d5888521d88f155c47c96a476e7fa7
sumitzerotohero@gmail.com <sumitzerotohero@gmail.com>
2025-01-17 11:14:00 +0000

    h

--- models/integration/rpt/order_summary.sql
+++ models/integration/rpt/order_summary.sql
@@ -1,15 +1,13 @@
-with final as (
-    select 
-        c.r_name as region_name, 
-        count(*) as order_count, 
-        sum(o.o_totalprice) as total_amount
-    from 
-        {{ source('my_project', 'orders') }} o
-    inner join 
-        {{ ref('customer_details') }} c 
-        on o.o_custkey = c.c_custkey
+create or replace transient table analytics.RPT.order_summary
+         as
+        (with final as (
+    select c.r_name as region_name, 
+    count(*) as order_count , 
+    sum(o.o_totalprice) as total_amount
+    from analytics.raw.orders o
+    inner join analytics.STG.customer_details c on o.o_custkey = c.c_custkey
     group by all
 )
 select *
-from final;
-
+from final
+        );
